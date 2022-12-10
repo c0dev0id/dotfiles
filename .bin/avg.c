f85c7da6dcfcf4efb71b7282065d98a41b3812c1
c0dev0id <sh+git@codevoid.de>
2022-12-10 11:54:09 +0100

    Update 2022-12-10 11:54         OpenBSD/amd64

--- .bin/avg.c
+++ .bin/avg.c
@@ -6,13 +6,13 @@ int main(int argc, const char *argv[])
 {
     unsigned long long no = 0;
     for (int i=1; i<argc; i++) {
-        no += strtoull(argv[i], NULL, 10) / 1000000;
+        no += strtoull(argv[i], NULL, 10);
     }
     if (argc < 2)
         printf("%lld\n", no);
 
     if (argc > 1)
-        printf("%.0lld\n", no / (argc - 1));
+        printf("%.0lld\n", no / (argc - 1) / 1000000);
 
     return 0;
 }
